9651d7f988c329e63de651607f1f7bd8ee50ce5d
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-02-16 21:28:41 +0630

    check the linter errors

--- solver.rb
+++ solver.rb
@@ -1,29 +1,27 @@
-class Solver 
-
- def factorial(n)
-  if n < 0
-   raise ArgumentError.new("Factorial only accepts positive integers")
-  elsif n == 0
-   1
-  else
-   (1..n).inject(:*)
+class Solver
+  def factorial(num)
+    if num.negative?
+      raise ArgumentError, 'Factorial only accepts positive integers'
+    elsif num.zero?
+      1
+    else
+      (1..num).inject(:*)
+    end
   end
- end
 
- def reverse(string)
-  string.reverse
- end
-
- def fizzbuzz(n)
-  if n % 3 == 0 && n % 5 == 0
-   return "fizzbuzz"
-  elsif n % 3 == 0
-   return 'fizz'
-  elsif n % 5 == 0
-   return 'buzz'
-   else
-    return n.to_s
+  def reverse(string)
+    string.reverse
   end
- end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
-
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,21 +1,19 @@
 require 'rspec'
-require './solver.rb'
+require './solver'
 
 describe Solver do
- 
- it "Return the factorial  1 if n = 0 " do
-  solver = Solver.new
-  expect(solver.factorial(0)).to eql(1)
- end
+  it 'Return the factorial  1 if num= 0 ' do
+    solver = Solver.new
+    expect(solver.factorial(0)).to eql(1)
+  end
 
- it "return sum of the positive integer from (1..n) when n is positve" do
- solver = Solver.new
- expect(solver.factorial(6)).to eql(720)
- end
-  
- it "return the reverse word for the given string" do
-  solver = Solver.new
-  expect(solver.reverse('car')).to eql ('rac')
- end
+  it 'return sum of the positive integer from (1..num) when num is positve' do
+    solver = Solver.new
+    expect(solver.factorial(6)).to eql(720)
+  end
 
+  it 'return the reverse word for the given string' do
+    solver = Solver.new
+    expect(solver.reverse('car')).to eql('rac')
+  end
 end
