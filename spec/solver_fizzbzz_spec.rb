c2b20ed3c86da32e36c56f7ea7b66a54c8f9a307
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-02-16 21:10:59 +0630

    create the test driven developement

--- spec/solver_fizzbzz_spec.rb
+++ spec/solver_fizzbzz_spec.rb
@@ -11,4 +11,14 @@ describe Solver do
   solver = Solver.new
  expect(solver.fizzbuzz(3)).to eq('fizz')
 end
+
+ it "When N is divisible by 5, return buzz" do
+  solver = Solver.new
+ expect(solver.fizzbuzz(5)).to eq('buzz')
+end
+
+ it "When N as a string" do
+  solver = Solver.new
+ expect(solver.fizzbuzz(11)).to eq('11')
+end
 end
