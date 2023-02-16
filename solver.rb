0920421e6659de6c53e9785d293eb78f3fa649fe
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-02-16 21:08:13 +0630

    create tcondition with test

--- solver.rb
+++ solver.rb
@@ -17,6 +17,12 @@ class Solver
  def fizzbuzz(n)
   if n % 3 == 0 && n % 5 == 0
    return "fizzbuzz"
+  elsif n % 3 == 0
+   return 'fizz'
+  elsif n % 5 == 0
+   return 'buzz'
+   else
+    return n.to_s
   end
  end
 end
--- spec/solver_fizzbzz_spec.rb
+++ spec/solver_fizzbzz_spec.rb
@@ -2,8 +2,13 @@ require 'rspec'
 require './solver.rb'
 
 describe Solver do
- it "check the method created or not" do
+ it "When N is divisible by 3 and 5, return fizzbuzz " do
   solver = Solver.new
   expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
  end
+
+ it "When N is divisible by 3, return fizz" do
+  solver = Solver.new
+ expect(solver.fizzbuzz(3)).to eq('fizz')
+end
 end
