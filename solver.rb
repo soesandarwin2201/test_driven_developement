920e0d9e1a8bce0e8f88e6b5c36246108249c88a
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-02-16 20:24:53 +0630

    Create the factorial method in the solver class

--- solver.rb
+++ solver.rb
@@ -1,2 +1,12 @@
 class Solver 
+
+ def factorial(n)
+  if n < 0
+   raise ArgumentError.new("Factorial only accepts positive integers")
+  elsif n == 0
+   1
+  else
+   (1..n).inject(:*)
+  end
+ end
 end
