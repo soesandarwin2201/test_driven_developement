4348c6e603f7f1a39849c8fb18f0e423d70208bd
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-02-16 20:20:56 +0630

    Check the factorial method to create

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,10 @@ require 'rspec'
 require './solver.rb'
 
 describe Solver do
+ describe '#factorial' do
+ it "Return the factorial  for the N" do
+  solver = Solver.new
+  expect(solver.factorial(0)).to eql(1)
+ end
+end
 end
